af8ea29dbf7a2d5e44bc01ba9721fcb8a2ed5fb9
Carlos GS <elmans8@gmail.com>
2025-03-08 14:17:53 -0600

    Apply code formatting to gradle.kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:8.9.0")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    classpath("app.cash.sqldelight:gradle-plugin:2.0.2")
 
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-        classpath("app.cash.sqldelight:gradle-plugin:2.0.2")
+    // Jetpack
+    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.8")
+    classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
 
-        // Jetpack
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.8")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+    // NOTE: Do not place your application dependencies here; they belong
+    // in the individual module build.gradle files
+  }
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
+tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
