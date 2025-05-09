0201fe9fac8912a9605a894fbe1b5044dd5b14b5
carlos_g <carlos_g@bitbucket.org>
2025-05-09 10:00:41 -0600

    Add pull to refresh logic to HomeScreen.kt

--- app/src/main/java/me/elmanss/melate/home/presentation/HomeScreenViewModel.kt
+++ app/src/main/java/me/elmanss/melate/home/presentation/HomeScreenViewModel.kt
@@ -3,7 +3,6 @@ package me.elmanss.melate.home.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -15,6 +14,7 @@ import kotlinx.coroutines.launch
 import logcat.logcat
 import me.elmanss.melate.home.domain.model.SorteoModel
 import me.elmanss.melate.home.domain.usecase.HomeUseCases
+import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(private val useCases: HomeUseCases) : ViewModel() {
@@ -28,13 +28,14 @@ class HomeScreenViewModel @Inject constructor(private val useCases: HomeUseCases
 
   fun launchFetchSorteos() {
     fetchJob?.cancel()
-    fetchJob =
-      viewModelScope.launch {
-        useCases.fetchSorteos().collectLatest {
-          logcat("HomeScreenVm") { it.toString() }
-          _state.update { s -> s.copy(sorteos = it) }
-        }
-      }
+    fetchJob = viewModelScope.launch { fetchSorteos() }
+  }
+
+  suspend fun fetchSorteos() {
+    useCases.fetchSorteos().collectLatest {
+      logcat("HomeScreenVm") { it.toString() }
+      _state.update { s -> s.copy(sorteos = it) }
+    }
   }
 
   fun launchSaveToFavorites(sorteoModel: SorteoModel) {
--- app/src/main/java/me/elmanss/melate/home/presentation/compose/HomeScreen.kt
+++ app/src/main/java/me/elmanss/melate/home/presentation/compose/HomeScreen.kt
@@ -6,25 +6,42 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.elmanss.melate.R
 import me.elmanss.melate.common.presentation.ui.compose.ui.component.MelateFab
 import me.elmanss.melate.common.presentation.ui.compose.ui.component.MelateTopBar
 import me.elmanss.melate.home.presentation.HomeScreenViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
 
   val uiState = viewModel.state.collectAsState()
   val sorteoState = rememberLazyListState()
+  val refreshState = rememberPullToRefreshState()
+  var isRefreshing by remember { mutableStateOf(false) }
+  val coroutineScope = rememberCoroutineScope()
 
   Scaffold(
     topBar = { MelateTopBar(title = R.string.app_name) },
@@ -32,11 +49,33 @@ fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
   ) {
     Column(modifier = Modifier.fillMaxWidth().padding(it)) {
       val sorteos = uiState.value.sorteos
-      LazyColumn(state = sorteoState) {
-        itemsIndexed(sorteos) { index, sorteo ->
-          Text(modifier = Modifier.padding(16.dp), text = sorteo.numeros.joinToString())
-          if (index < sorteos.lastIndex) {
-            HorizontalDivider(thickness = Dp.Hairline)
+      PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+          isRefreshing = true
+          coroutineScope.launch {
+            delay(1500)
+            viewModel.fetchSorteos()
+            isRefreshing = false
+          }
+        },
+        state = refreshState,
+        indicator = {
+          Indicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            isRefreshing = isRefreshing,
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            state = refreshState,
+          )
+        },
+      ) {
+        LazyColumn(state = sorteoState) {
+          itemsIndexed(sorteos) { index, sorteo ->
+            Text(modifier = Modifier.padding(16.dp), text = sorteo.numeros.joinToString())
+            if (index < sorteos.lastIndex) {
+              HorizontalDivider(thickness = Dp.Hairline)
+            }
           }
         }
       }
