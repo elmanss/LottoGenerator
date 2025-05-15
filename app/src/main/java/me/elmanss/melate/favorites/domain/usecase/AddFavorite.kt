e4a202f955ef5c969fb4531be4fb52959df54bc0
carlos_g <carlos_g@bitbucket.org>
2025-05-15 10:24:18 -0600

    Add icon to distinguish manually / randomly created draws in ListFavoritesScreen.kt

--- app/src/main/java/me/elmanss/melate/favorites/domain/usecase/AddFavorite.kt
+++ app/src/main/java/me/elmanss/melate/favorites/domain/usecase/AddFavorite.kt
@@ -1,11 +1,11 @@
 package me.elmanss.melate.favorites.domain.usecase
 
+import javax.inject.Inject
 import me.elmanss.melate.common.data.repository.FavoritosRepository
 import me.elmanss.melate.favorites.domain.model.FavoritoModel
-import javax.inject.Inject
 
 class AddFavorite @Inject constructor(private val repository: FavoritosRepository) {
   suspend operator fun invoke(model: FavoritoModel) {
-    repository.createFavoritos(model.sorteo)
+    repository.createFavoritos(model.sorteo, model.origin)
   }
 }
--- app/src/main/java/me/elmanss/melate/favorites/presentation/list/compose/ListFavoritesScreen.kt
+++ app/src/main/java/me/elmanss/melate/favorites/presentation/list/compose/ListFavoritesScreen.kt
@@ -1,7 +1,10 @@
 package me.elmanss.melate.favorites.presentation.list.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,7 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -54,15 +59,32 @@ fun ListFavoritesScreen(
 
       LazyColumn(state = sorteoState) {
         itemsIndexed(favs) { index, fav ->
-          Text(
+          Row(
             modifier =
               Modifier.fillMaxWidth()
                 .padding(16.dp)
-                .combinedClickable(onClick = {}, onLongClick = { viewModel.showWarning(fav) }),
-            text =
-              if (fav.origin == FavOrigin.Random) fav.sorteo.removePrefix("[").removeSuffix("]")
-              else fav.sorteo,
-          )
+                .combinedClickable(onClick = {}, onLongClick = { viewModel.showWarning(fav) })
+          ) {
+            Text(
+              text =
+                if (fav.origin == FavOrigin.Random) fav.sorteo.removePrefix("[").removeSuffix("]")
+                else fav.sorteo
+            )
+            Row(
+              modifier = Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.End,
+              verticalAlignment = Alignment.CenterVertically,
+            ) {
+              Image(
+                painter =
+                  painterResource(
+                    if (fav.origin == FavOrigin.Random) R.drawable.cellphone
+                    else R.drawable.human_edit
+                  ),
+                contentDescription = "Origin icon",
+              )
+            }
+          }
           if (index < favs.lastIndex) {
             HorizontalDivider(thickness = Dp.Hairline)
           }
