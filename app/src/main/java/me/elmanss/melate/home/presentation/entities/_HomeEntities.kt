d8337a602f29a30e815ef4a7e298a88493fe8c7c
carlos_g <carlos_g@bitbucket.org>
2026-02-17 15:20:16 -0600

    feat: Reorder HomeUiEvent sealed class
    This commit reorders the data classes within the `HomeUiEvent` sealed class for better logical grouping. It also removes an extraneous `{}` from the `HomeScreenState` data class definition. No functional changes are introduced.

--- app/src/main/java/me/elmanss/melate/home/presentation/entities/_HomeEntities.kt
+++ app/src/main/java/me/elmanss/melate/home/presentation/entities/_HomeEntities.kt
@@ -5,21 +5,21 @@ import me.elmanss.melate.home.domain.model.SorteoModel
 sealed class HomeUiEvent {
   data object RefreshSorteosEvent : HomeUiEvent()
 
-  data class ClickSorteoEvent(val sorteo: SorteoModel) : HomeUiEvent()
-
-  data class ClickAddSorteoEvent(val sorteo: SorteoModel) : HomeUiEvent()
-
   data object DismissAddSorteoEvent : HomeUiEvent()
 
-  data class EnableMultiSelectEvent(val sorteo: SorteoModel, val index: Int) : HomeUiEvent()
-
   data object DisableMultiSelectEvent : HomeUiEvent()
-
-  data class SelectSorteoEvent(val sorteo: SorteoModel, val index: Int) : HomeUiEvent()
 
   data object ClickConfirmMultiSelectEvent : HomeUiEvent()
 
   data object ClickGoToFavsEvent : HomeUiEvent()
+
+  data class ClickSorteoEvent(val sorteo: SorteoModel) : HomeUiEvent()
+
+  data class ClickAddSorteoEvent(val sorteo: SorteoModel) : HomeUiEvent()
+
+  data class EnableMultiSelectEvent(val sorteo: SorteoModel, val index: Int) : HomeUiEvent()
+
+  data class SelectSorteoEvent(val sorteo: SorteoModel, val index: Int) : HomeUiEvent()
 }
 
 sealed class HomeScreenSideEffect {
@@ -32,4 +32,4 @@ data class HomeScreenState(
   val sorteos: List<SorteoModel> = emptyList(),
   val saveFaveDialogDisplayed: SorteoModel? = null,
   val multiSelectModeEnabled: Boolean = false,
-) {}
+)
